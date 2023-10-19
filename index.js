cd3ee6fc1dae7838270243daa2037163f24247e7
ROCKHOSSAIN <rockyhossain736@gmail.com>
2023-10-19 23:20:54 +0600

    the 6 differentscar api api has been added in database

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config(); 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+//addCars
+//E1wKnAF3g11wJrqp
+app.use(cors())
+app.use(express.json());
+// console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vh2cr5s.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const carsCollection  =client.db('carsData').collection('carStore')
+
+    app.post('/allcar',async(req,res)=>{
+        const newCar = req.body;
+        console.log(newCar)
+        const result = await carsCollection.insertOne(newCar)
+        res.send(result)
+    })
+    app.get('/allcar',async(req,res)=>{
+        const cursor = carsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+//middleware
+
+
+app.get('/',(req,res)=>{
+    res.send('Cars product making server is running');
+})
+app.listen(port,()=>{
+    console.log(`Cars server is running on the port :${port}`)
+})
